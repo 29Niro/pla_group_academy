614d7b9cec477649f4acccac701b5411d96ec92e
Pushparaj Niroshan <29niro@gmail.com>
2024-12-10 21:52:38 +0530

    use dummy data

--- lib/custom_listview.dart
+++ lib/custom_listview.dart
@@ -1,7 +1,51 @@
 import 'package:flutter/material.dart';
 
 class CustomListview extends StatelessWidget {
-  const CustomListview({super.key});
+  CustomListview({super.key});
+
+  final List<Map<String, String>> friendRequests = [
+    {
+      'name': 'John Doe',
+      'mutualFriends': '5 Mutual Friends',
+      'image': 'https://i.pravatar.cc/150?img=1'
+    },
+    {
+      'name': 'Jane Smith',
+      'mutualFriends': '2 Mutual Friends',
+      'image': 'https://i.pravatar.cc/150?img=2'
+    },
+    {
+      'name': 'Alice Johnson',
+      'mutualFriends': '10 Mutual Friends',
+      'image': 'https://i.pravatar.cc/150?img=3'
+    },
+    {
+      'name': 'Bob Brown',
+      'mutualFriends': '8 Mutual Friends',
+      'image': 'https://i.pravatar.cc/150?img=4'
+    },
+    {
+      'name': 'Charlie Davis',
+      'mutualFriends': '1 Mutual Friend',
+      'image': 'https://i.pravatar.cc/150?img=5'
+    },
+    {
+      'name': 'Diana Evans',
+      'mutualFriends': '15 Mutual Friends',
+      'image': 'https://i.pravatar.cc/150?img=6'
+    },
+    {
+      'name': 'Eve Wilson',
+      'mutualFriends': '4 Mutual Friends',
+      'image': 'https://i.pravatar.cc/150?img=7'
+    },
+    {
+      'name': 'Frank Harris',
+      'mutualFriends': '20 Mutual Friends',
+      'image': 'https://i.pravatar.cc/150?img=8'
+    },
+    {'name': 'Gee', 'mutualFriends': '7 Mutual Friends', 'image': ''},
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +54,10 @@ class CustomListview extends StatelessWidget {
         title: const Text("Custom List view"),
       ),
       body: ListView.builder(
-          itemCount: 10,
+          itemCount: friendRequests.length,
           itemBuilder: (context, index) {
+            final friendRequest = friendRequests[index];
+
             return Padding(
               padding: const EdgeInsets.all(8.0),
               child: Container(
@@ -20,8 +66,13 @@ class CustomListview extends StatelessWidget {
                 child: Container(
                   child: Row(
                     children: [
-                      const CircleAvatar(
+                      CircleAvatar(
                         minRadius: 50,
+                        backgroundImage: friendRequest['image'] != null &&
+                                friendRequest['image']!.isNotEmpty
+                            ? NetworkImage(friendRequest['image']!)
+                            : const AssetImage("assets/profile_pic.jpg"),
+                            // condition ? function 1 : condition 2 ? function 2 : function 3
                       ),
                       const SizedBox(
                         width: 10,
@@ -29,8 +80,8 @@ class CustomListview extends StatelessWidget {
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          const Text("Name"),
-                          const Text("24 mutual friends"),
+                          Text(friendRequest['name']!),
+                          Text(friendRequest['mutualFriends']!),
                           Row(
                             children: [
                               ElevatedButton(
